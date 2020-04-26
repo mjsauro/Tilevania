d286a51092847c529a8fda7f15ed3dbdc204d348
mjsauro <mjsauro@gmail.com>
2020-04-26 17:47:12 -0500

    player now moves left and right and moved files around

--- Tilevania/Assets/Scripts/Player.cs
+++ Tilevania/Assets/Scripts/Player.cs
@@ -1,25 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
-    [SerializeField] float runSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private const string Horizontal = "Horizontal";
+	private Rigidbody2D _myRigidBody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Run();
-    }
+	[SerializeField] private float runSpeed = 1f;
 
-    private void Run()
-    {
-        _rigidbody2D.velocity.x = 1f;
-    }
+	// Start is called before the first frame update
+	private void Start()
+	{
+		_myRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		Run();
+	}
+
+	private void Run()
+	{
+		float controlThrow = CrossPlatformInputManager.GetAxis(Horizontal); //value is -1 to +1
+		var playerVelocity = new Vector2(controlThrow * runSpeed, _myRigidBody.velocity.y);
+		_myRigidBody.velocity = playerVelocity;
+	}
 }

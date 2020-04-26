27b8b5640cb78751264c85d3bbea3bf53bf920b2
mjsauro <mjsauro@gmail.com>
2020-04-26 17:23:50 -0500

    imported standard assets

---
+++ Tilevania/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField] float runSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        _rigidbody2D.velocity.x = 1f;
+    }
+}

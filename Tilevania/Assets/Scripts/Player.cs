5c6697f9e909ff5208a8e6adef74a42351db1701
mjsauro <mjsauro@gmail.com>
2020-04-26 18:01:41 -0500

    player's sprite now flips left and right depending on the direction

--- Tilevania/Assets/Scripts/Player.cs
+++ Tilevania/Assets/Scripts/Player.cs
@@ -18,6 +18,7 @@ public class Player : MonoBehaviour
 	private void Update()
 	{
 		Run();
+		FlipSprite();
 	}
 
 	private void Run()
@@ -25,5 +26,15 @@ public class Player : MonoBehaviour
 		float controlThrow = CrossPlatformInputManager.GetAxis(Horizontal); //value is -1 to +1
 		var playerVelocity = new Vector2(controlThrow * runSpeed, _myRigidBody.velocity.y);
 		_myRigidBody.velocity = playerVelocity;
+	}
+
+	private void FlipSprite()
+	{
+		bool playerHasHorizontalSpeed = Mathf.Abs(_myRigidBody.velocity.x) > Mathf.Epsilon;
+
+		if (playerHasHorizontalSpeed)
+		{
+			transform.localScale = new Vector2(Mathf.Sign(_myRigidBody.velocity.x), 1f);
+		}
 	}
 }

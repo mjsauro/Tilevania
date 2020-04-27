39588b07a04e9b5b2b603603e187e6db8d857c71
mjsauro <mjsauro@gmail.com>
2020-04-26 22:00:48 -0500

    added ladder and probably messed this up

--- Tilevania/Assets/Scripts/Player.cs
+++ Tilevania/Assets/Scripts/Player.cs
@@ -8,12 +8,14 @@ public class Player : MonoBehaviour
 	//Cached component references
 	private Rigidbody2D _myRigidBody;
 	private Animator _myAnimator;
+	private Collider2D _myCollider;
 
 	//Config
 	[SerializeField] private float runSpeed = 5f;
 	[SerializeField] private float jumpSpeed = 5f;
+	[SerializeField] private float climbSpeed = 5f;
 	private static readonly int Running = Animator.StringToHash("Running");
-
+	private static readonly int Climbing = Animator.StringToHash("Climbing");
 
 	//Messages
 	
@@ -24,6 +26,8 @@ public class Player : MonoBehaviour
 	{
 		_myRigidBody = GetComponent<Rigidbody2D>();
 		_myAnimator = GetComponent<Animator>();
+		_myCollider = GetComponent<Collider2D>();
+		Debug.Log(LayerMask.GetMask("Default", "Ground"));
 	}
 
 	// Update is called once per frame
@@ -31,8 +35,10 @@ public class Player : MonoBehaviour
 	{
 		Run();
 		Jump();
+		Climb();
 		FlipSprite();
 	}
+	
 
 	private void Run()
 	{
@@ -43,9 +49,29 @@ public class Player : MonoBehaviour
 		bool playerHasHorizontalSpeed = PlayerHasHorizontalSpeed();
 		_myAnimator.SetBool(Running, playerHasHorizontalSpeed);
 	}
+	
+	private void Climb()
+	{
+		if (_myCollider.IsTouchingLayers(LayerMask.GetMask("Default", "Ladder")))
+		{
+			float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); //value is -1 to +1
+			var playerVelocity = new Vector2(_myRigidBody.velocity.x, controlThrow * climbSpeed);
+			_myRigidBody.velocity = playerVelocity;
+			bool playerHasVerticalSpeed = PlayerHasVerticalSpeed();
+			_myAnimator.SetBool(Climbing, playerHasVerticalSpeed);
+		}
+		else
+		{
+			_myAnimator.SetBool(Climbing, false);
+
+		}
+	}
+
 
 	private void Jump()
 	{
+		if (!_myCollider.IsTouchingLayers(LayerMask.GetMask("Default", "Ground"))) return;
+		
 		if (CrossPlatformInputManager.GetButtonDown("Jump"))
 		{
 			Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
@@ -53,6 +79,7 @@ public class Player : MonoBehaviour
 			_myRigidBody.velocity += jumpVelocityToAdd;
 		}
 	}
+	
 
 	private void FlipSprite()
 	{
@@ -67,5 +94,10 @@ public class Player : MonoBehaviour
 	private bool PlayerHasHorizontalSpeed()
 	{
 		return  Mathf.Abs(_myRigidBody.velocity.x) > Mathf.Epsilon;
+	}
+	
+	private bool PlayerHasVerticalSpeed()
+	{
+		return  Mathf.Abs(_myRigidBody.velocity.y) > Mathf.Epsilon;
 	}
 }
